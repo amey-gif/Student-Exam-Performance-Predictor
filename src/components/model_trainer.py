f357a35e6990fe6f5b2e32f909512796259a22a0
matwa@DESKTOP-HD15CCO <ameyparab1105@gmail.com>
2026-01-29 16:52:31 +0530

    hyperparameters training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -54,8 +54,52 @@ class ModelTrainer:
                 "CatBoost Classifier": CatBoostClassifier(verbose=False),
                 "AdaBoost Classifier": AdaBoostClassifier(),
             }
+            
+            params = {
+                "Decision Tree": {
+                    "criterion": ["gini", "entropy", "log_loss"],
+                    "max_depth": [None, 5, 10, 20]              
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2']
+                },
+                "Random Forest": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2', None]
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['sqrt','log2', None],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Classifier": {
+                    'n_neighbors':[5,7,9,11,13,15],
+                    # 'weights':['uniform', 'distance'],
+                    # 'algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "XGB Regressor": {
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost Classifier": {
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,0.05],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
 
-            model_report: dict =evalute_models(x_train=x_train, y_train=y_train,x_test=x_test, y_test=y_test, models=models)
+            }
+
+
+
+            model_report: dict =evalute_models(x_train=x_train, y_train=y_train,x_test=x_test, y_test=y_test, models=models, param=params)
 
             best_model_score = max(sorted(model_report.values()))
 
